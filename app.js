58303c07bfc67f73adc6da300ea78fa8152cdb7b
Suray-naz <129758167+Suray-naz@users.noreply.github.com>
2023-06-21 15:00:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//?  1 ile 100 aerasinda bir sayi tut
+//?  variables tanimlama
+//?  Check butonuna basildiginda kontrolleri yap
+//!  Eger inputtan tahmin girilmediyse kullaniciya uyari ver
+//!  Eger rastgele sayi esitse inputan alinan sayidegerine
+//?  tebrikler bildiniz
+//?  sakli sayi =görünür
+//!  eger rastgelesayi esit degilse inputtan alinan degere
+//?  tahmin hakkini azalmali
+//!  eger tahmin hakk >0 ise --------->
+//!   inputtan alinan deger > rastgele sayidan büyük ise-> tahminini azalt : tahminini artir
+//!  eger tahmin hakki < 0 degilse
+//?  Üzgünüm kaybettiniz
+//?  tekrar tusuna basinca oyunu basa al( tahmin hakkini düzelt,
+//?  tekrar rastgele sayi üretmeli,kontrol tusunu tekrar aktif yapmaliyiz,
+//?  sakli sayiya soru isareti  atamaliyiz,tahmin inputuna bos deger atamaliyiz,mesaji eski haline almaliyiz)
+
+/* -------------------------------------------------------------------------- */
+/*                      1 ile 100 aerasinda bir sayi tut                      */
+/* -------------------------------------------------------------------------- */
